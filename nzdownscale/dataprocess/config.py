b75dd782411ec1f81869ff536db36c833fe74a56
Emily ORiordan <emily@bodekerscientific.com>
2024-01-18 14:40:21 +1300

    Added data filepaths for Emily

--- nzdownscale/dataprocess/config.py
+++ nzdownscale/dataprocess/config.py
@@ -4,15 +4,18 @@ VARIABLE_OPTIONS = ['temperature', 'precipitation']
 
 DATA_PATHS = {
     'ERA5': {
-        'parent': 'data/ERA5-land',
+        # 'parent': 'data/ERA5-land',
+        'parent': '/mnt/datasets/ERA5/NZ_land',
     },    
     'topography': {
-        'parent': 'data/topography',
-        'file': 'data/topography/nz_elevation_200m.nc',
+        # 'parent': 'data/topography',
+        # 'file': 'data/topography/nz_elevation_200m.nc',
+        'parent': 'experiments/deepsensor/data/data/topography',
+        'file': 'experiments/deepsensor/data/topography/nz_elevation_200m.nc',
     },    
     'stations': {
-        'parent': 'data/stations',
-        #'parent': '/mnt/datasets/NationalClimateDatabase/NetCDFFilesByVariableAndSite/Hourly',
+        # 'parent': 'data/stations',
+        'parent': '/mnt/datasets/NationalClimateDatabase/NetCDFFilesByVariableAndSite/Hourly',
     },
 }
 
