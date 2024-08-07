0a903f09daae227a40d08b294918b4586f14b751
Emily ORiordan <emily@bodekerscientific.com>
2024-08-07 13:24:21 +1200

    Adding functions to look at distributions of variables

--- experiments/data_explore/vis_era5.py
+++ experiments/data_explore/vis_era5.py
@@ -11,31 +11,103 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cf
 import seaborn as sns
+import scipy.stats as stats
+from scipy.stats import gamma, bernoulli
+from scipy.optimize import minimize
 
 from nzdownscale.dataprocess.era5 import ProcessERA5
 from nzdownscale.dataprocess.utils import PlotData
 
 #%% Load ERA5 data
 
-var = 'temperature'
-#var = 'precipitation'
 era5 = ProcessERA5()
+plotnz = PlotData()
 
-#%% Load all ERA5 data
-
-#ds = era5.load_ds(var)  # ~2min 
+# %%
+def neg_log_likelihood(params, non_zero_data):
+    alpha, beta = params[0], params[1]
+    return -np.sum(gamma.logpdf(non_zero_data, alpha, scale=1/beta))
 
 #%% Load specific year only
 
-ds = era5.load_ds(var, 2000)
-da = era5.ds_to_da(ds, var)
+for var in [
+        'temperature',
+        'precipitation',
+        '10m_u_component_of_wind',
+        '10m_v_component_of_wind',
+        'surface_pressure',
+        'surface_solar_radiation_downwards',
+        ]:
 
-#%% Plot timeslice
+    ds = era5.load_ds(var, 2000)
+    da = era5.ds_to_da(ds, var)
 
-plotnz = PlotData()
-ax = plotnz.nz_map_with_coastlines()
-da_to_plot = da.isel(time=0)
-da_to_plot.plot()
-plt.title(f'ERA5-land: {var}\n{da_to_plot["time"].values}')
-plt.savefig('./tmp/fig.png')
-plt.show()
+    # Plot timeslice
+    # ax = plotnz.nz_map_with_coastlines()
+    # da_to_plot = da.isel(time=0)
+    # da_to_plot.plot()
+    # plt.title(f'ERA5-land: {var}\n{da_to_plot["time"].values}')
+    # # plt.savefig('./tmp/fig.png')
+    # plt.show()
+
+    # Plot histogram of all values
+    fig, ax = plt.subplots()
+    da.plot.hist(ax=ax, bins=100, density=True, label=f'ERA5 2000 {var} histogram')
+    print('Plotted histogram')
+
+    # # plot gaussian
+    mean = da.mean().values
+    std = da.std().values
+    x = np.linspace(mean - 3*std, mean + 3*std, 100)
+
+    ax.plot(x, stats.norm.pdf(x, mean, std), c='r', 
+            label = f'N({mean:.2f}, {std:.2f})')
+    print('Plotted gaussian')
+
+    plt.title(f'ERA5-land: {var} histogram')
+    # # plot bernoulli-gamma
+   
+    # Aggregate data across latitudes and longitudes
+    data = da.mean(dim=['latitude', 'longitude'])
+
+    # Calculate Bernoulli parameter (probability of success)
+    p = data.mean().values
+
+    # Calculate Gamma parameters using method of moments
+    def gamma_params(data):
+        mean = np.mean(data)
+        var = np.var(data)
+        shape = mean**2 / var
+        scale = var / mean
+        return shape, scale
+
+    # Extract data values for calculation
+    data_values = data.values.flatten()
+    shape, scale = gamma_params(data_values)
+
+    # Plot histogram of the data points
+    # fig, ax = plt.subplots()
+    # ax.hist(data_values, bins=100, density=True, alpha=0.5, label='Data Histogram')
+
+    # Plot Bernoulli distribution
+    # bernoulli_dist = bernoulli(p)
+    # x_bernoulli = np.array([0, 1])
+    # y_bernoulli = bernoulli_dist.pmf(x_bernoulli)
+    # ax.stem(x_bernoulli, y_bernoulli, 'r', markerfmt='ro', label=f'Bernoulli PMF p={p:.2f}', basefmt=" ")
+
+    # Plot Gamma distribution
+    x_gamma = np.linspace(0, data_values.max(), 100)
+    y_gamma = gamma.pdf(x_gamma, a=shape, scale=scale)
+    ax.plot(x_gamma, y_gamma, 'g-', label=f'Gamma PDF shape={shape:.2f}, scale={scale:.2f}')
+
+    # Add titles and labels
+    # ax.set_title(f'Fitted Bernoulli-Gamma Distributions for {var}')
+    ax.set_xlabel('Data Values')
+    ax.set_ylabel('Density')
+    ax.legend()
+
+
+    plt.show()
+
+
+# %%
--- experiments/data_explore/vis_stations.py
+++ experiments/data_explore/vis_stations.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import seaborn as sns
+from scipy.stats import gamma
 
 from nzdownscale.dataprocess.stations import ProcessStations
 from nzdownscale.dataprocess.utils import PlotData
@@ -17,8 +18,12 @@ from nzdownscale.dataprocess.config import VAR_STATIONS
 
 #%% get station file paths
 
-#var = 'precipitation'
-var = 'temperature'
+# var = 'precipitation'
+# var = 'temperature'
+# var = 'pressure'
+# var = 'windspeed'
+# var = 'winddirection'
+var = 'humidity'
 process_stations = ProcessStations()
 all_stations = process_stations.get_path_all_stations(var)
 print(len(all_stations))
@@ -86,6 +91,46 @@ df = pd.concat(df_list)
 station_raw_df = df.reset_index().set_index(['time', 'latitude', 'longitude']).sort_index()
 print(station_raw_df)
 
+#%% Look at distribution of station values
+
+df_var = VAR_STATIONS[var]['var_name']
+
+station_raw_df_values = station_raw_df[df_var]
+station_raw_df_values = station_raw_df_values.dropna()
+# Take a random sample of 5000 values
+station_raw_df_values = station_raw_df_values.sample(5000)
+
+# Plot histogram of the data points
+fig, ax = plt.subplots()
+ax.hist(station_raw_df_values, density=True, bins=100)
+
+# Plot Gaussian distribution
+mean, std = station_raw_df_values.mean(), station_raw_df_values.std()
+x = np.linspace(mean - 3*std, mean + 3*std, 100)
+y = (1/(std * np.sqrt(2*np.pi))) * np.exp(-0.5*((x-mean)/std)**2)
+ax.plot(x, y, color='r', label='Gaussian Distribution')
+
+# Plot Gamma distribution
+def gamma_params(data):
+        mean = np.mean(data)
+        var = np.var(data)
+        shape = mean**2 / var
+        scale = var / mean
+        return shape, scale
+
+shape, scale = gamma_params(station_raw_df_values)
+x_gamma = np.linspace(0, station_raw_df_values.max(), 100)
+y_gamma = gamma.pdf(x_gamma, a=shape, scale=scale)
+ax.plot(x_gamma, y_gamma, 'g-', label=f'Gamma PDF shape={shape:.2f}, scale={scale:.2f}')
+
+
+ax.legend()
+ax.set_title(f'{var.title()} Station Data Distribution')
+# ax.set_xlim([0, 10])
+plt.show()
+
+
+
 #%% 
 # Generate auxiliary dataset of x1/x2 coordinates to break translation equivariance in the model's CNN
 # to enable learning non-stationarity
@@ -93,3 +138,4 @@ x1x2_ds = construct_x1x2_ds(aux_ds)
 aux_ds['x1_arr'] = x1x2_ds['x1_arr']
 aux_ds['x2_arr'] = x1x2_ds['x2_arr']
 aux_ds
+# %%
